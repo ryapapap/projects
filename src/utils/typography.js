0bce99d5aa84a0bfd5263e4af1cc7a2a612d886e
Ryan Patterson <ryan@Ryans-MacBook-Air.local>
2018-04-01 16:29:50 -0700

    whoops

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,9 +1,5 @@
 import Typography from 'typography'
 import theme from 'typography-theme-lincoln'
-// import theme from 'typography-theme-ocean-beach'
-// import theme from 'typography-theme-twin-peaks'
-// import theme from 'typography-theme-judah'
-// import theme from 'typography-theme-sutro'
 
 theme.overrideThemeStyles = () => ({
   'a.gatsby-resp-image-link': {
